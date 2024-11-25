b7a12af0ea7972d5779c6a99233f0085b8497e20
daguttt <dagutmu667@gmail.com>
2024-11-25 18:42:34 -0500

    feat: create base reports controller endpoints

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,10 @@
+class ReportsController < ApplicationController
+  def create
+  end
+
+  def get_history
+  end
+
+  def schedule
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post "/events/:event_id/reports" => "reports#create", as: :event_reports
+  get "/events/:event_id/reports/get_history" => "reports#get_history", as: :event_reports_history
+  put "/events/:event_id/reports/schedule" => "reports#schedule", as: :event_reports_schedule
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
