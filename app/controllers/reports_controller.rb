27f2f07ed157ed6781f847bdf2cbdb34261772f5
daguttt <dagutmu667@gmail.com>
2024-11-26 19:50:56 -0500

    chore: add summary and tags for reports endpoints

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,17 @@
 class ReportsController < ApplicationController
+  # @summary Creates an attendance or ticket report
+  # @tags Reports
   def create
+    render json: params
   end
 
+  # @summary Get an event's reports history
+  # @tags Reports
   def get_history
   end
 
+  # @summary Schedule the generation of a report
+  # @tags Reports
   def schedule
   end
 end
