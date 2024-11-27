96276386d0ebdb5c4250d1c95e16d516e7da2849
Daniel Gutierrez Muñoz <70865733+daguttt@users.noreply.github.com>
2024-11-27 10:31:43 -0500

    feat: add Open API documentation with OASRails (#5)
    * feat: set up open api documentation with `oas_rails` gem
    
    * chore: add summary and tags for reports endpoints
    
    * fix: remove returning of params in create report endpoint

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,16 @@
 class ReportsController < ApplicationController
+  # @summary Creates an attendance or ticket report
+  # @tags Reports
   def create
   end
 
+  # @summary Get an event's reports history
+  # @tags Reports
   def get_history
   end
 
+  # @summary Schedule the generation of a report
+  # @tags Reports
   def schedule
   end
 end
