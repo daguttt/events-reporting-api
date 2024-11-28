a08b03183e2d2fa7d5eca2372526bebc226fb34c
William Morales <163943057+MoralesWilll@users.noreply.github.com>
2024-11-28 10:58:01 -0500

    feat: create a job for scheduling reports and the controller endpoint for it (#8)
    * feat: add sidekiq gems
    
    * feat: add routes and config for sidekiq use
    
    * feat: create a job for scheduling reports and the controller for it
    
    * fix: change burned-in secret to be sourced from ENV variable
    
    ---------
    
    Co-authored-by: daguttt <dagutmu667@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   get "/" => redirect("/docs")
 
@@ -10,7 +12,10 @@ Rails.application.routes.draw do
   post "/events/:event_id/reports" => "reports#create", as: :event_reports
   get "/events/:event_id/reports/get_history" => "reports#get_history", as: :event_reports_history
   put "/events/:event_id/reports/schedule" => "reports#schedule", as: :event_reports_schedule
+  put "/events/deletejob" => "reports#delete", as: :events_job_delete
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
