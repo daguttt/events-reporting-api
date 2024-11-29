f1906a87b468dcae67eae61db9eb29ce7775a761
William Morales <163943057+MoralesWilll@users.noreply.github.com>
2024-11-28 19:39:35 -0500

    feat: integrate schedue endpoint with report ticket service (#10)
    * feat: call and validate event
    
    * feat: valdidate and require schedule report params
    
    * refactor: delete unused endpoint
    
    * refactor: clean logic in schedule endpoint
    
    * feat: integrate schedue endpoint with report ticket service
    
    * fix: add missing `format` param to schedule next ticket report generation
    
    * refactor: use just one call to the `require` method of params
    
    * refactor: remove unnecessary loading of the "prawn" gem
    
    * feat: allow scheduling multiple report of the same event in different formats
    
    ---------
    
    Co-authored-by: daguttt <dagutmu667@gmail.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -70,59 +70,35 @@ class ReportsController < ApplicationController
   def get_history
   end
 
-  def delete
-    event_id = 1
-    # Loop through all jobs in the Scheduled Set
-    Sidekiq::ScheduledSet.new.each do |job|
-      # Check if the job's arguments match the ones you're looking for
-      if job.args[0] == event_id
-        # Found a matching job, so you can delete it
-        job.delete
-        logger.info("Job with JID #{job.jid} has been deleted. Event ID: #{event_id}")
-        return job.jid  # Optionally return the JID for reference
-      end
-    end
-  end
-
-
   # @summary Schedule the generation of a report
   # @tags Reports
   def schedule
-    event_id = 1
-    frequency = "weekly"
-
+    event_id = schedule_report_params[:event_id]
+    frequency = schedule_report_params[:frequency]
+    format = schedule_report_params[:format]
     # Check if the event exists
-    # event = Event.find_by(id: event_id)
-    # unless event
-    #   render json: { error: "Event not found" }, status: :not_found
-    #   return
-    # end
-
-    # # Check if the frequency is valid
-    # unless %w[daily weekly monthly].include?(frequency)
-    #   render json: { error: "Invalid frequency" }, status: :unprocessable_entity
-    #   return
-    # end
-
-    case frequency
-
-    when "daily"
-      # Schedule the job to run again every day at the same time (i.e., 24 hours later).
-      ReportSchedulerJob.perform_async(event_id, "daily")
-    when "weekly"
-      # Schedule the job to run again every week at the same day and time.
-      ReportSchedulerJob.perform_async(event_id, "weekly")
-    when "monthly"
-      # Schedule the job to run again every month at the same day and time.
-      ReportSchedulerJob.perform_async(event_id, "monthly")
-    else
-      # Handle the case where an unsupported frequency is passed
-      logger.error("Unsupported frequency: #{frequency} for event ID #{event_id}. Job will not be rescheduled.")
+    event = EventsService.find_by_id(event_id)
+    unless event
+      render json: { error: "Event not found" }, status: :not_found
+      return
     end
+
+    # Check if the frequency is valid
+    unless %w[daily weekly monthly].include?(frequency)
+      render json: { error: "Invalid frequency" }, status: :unprocessable_entity
+      return
+    end
+
+    ReportSchedulerJob.perform_async(event_id, frequency, format)
   end
 
   private
   def report_params
     params.permit(:type, :user_id, :format, :event_id, report: [ :format ])
+  end
+
+  def schedule_report_params
+    params.require([ :event_id, :frequency, :user_id, :format ])
+    params.permit(:event_id, :frequency, :user_id, :format, report: {})
   end
 end
