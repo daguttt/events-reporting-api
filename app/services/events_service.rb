2843bf84c0b50e9478f600ac6f69fe1f79af520d
daguttt <dagutmu667@gmail.com>
2024-11-29 21:27:51 -0500

    fix: change monolith endpoint to fetch an event and adapt it for production

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -1,13 +1,13 @@
 require "net/http"
 
 class EventsService
-  MONOLITH_URL = "#{ENV.fetch("MONOLITH_URL")}/api/v1"
+  MONOLITH_URL = "#{ENV.fetch("MONOLITH_URL")}/es"
   def self.find_by_id(id)
     uri = URI.parse(MONOLITH_URL)
     response = nil
     begin
-      Net::HTTP.start(uri.host, uri.port, open_timeout: 1, read_timeout: 2) do |http|
-        response = http.get("#{uri.path}/events/#{id}")
+      Net::HTTP.start(uri.host, use_ssl: uri.scheme == "https", open_timeout: 1, read_timeout: 2) do |http|
+        response = http.get("#{uri.path}/events/#{id}.json")
       end
     rescue SocketError
       Rails.logger.error("Could not connect to the monolith")
@@ -19,7 +19,6 @@ class EventsService
 
     return nil if response.nil? || response.is_a?(Net::HTTPNotFound)
 
-    parsed_body = JSON.parse(response.body)
-    parsed_body["data"]
+    JSON.parse(response.body)
   end
 end
