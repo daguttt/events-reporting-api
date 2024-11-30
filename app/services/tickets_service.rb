b25c0a7e080d9b7244cff70f62ec771f1fc482f2
Diegojape <78570681+diego97jaramillo@users.noreply.github.com>
2024-11-30 13:59:59 -0500

    Feat/tickets report connecting 2endpoint (#13)
    * feat: added the validation for tickets report
    
    * fix: the Tickets_service calls corrected in the controller
    
    * feat: added validation to the model
    
    * fix: the validation were corrected
    
    * fix: the connection to the endpoint is modified to read the domain from ENV
    
    * refactor: add guard clause to raise if event does not exist
    
    * style: separate `SECRET` env variable
    
    * refactor: use `TICKETS_URL` env variable to fetch event tickets summary
    
    The variable was already defined
    
    ---------
    
    Co-authored-by: daguttt <dagutmu667@gmail.com>

--- app/services/tickets_service.rb
+++ app/services/tickets_service.rb
@@ -6,64 +6,61 @@ require "prawn/table"
 
 class TicketsService
   def self.create_report(ticket_params)
-    # raw_data = get_ticket_summary
-    # total_tickets = raw_data["data"].total_tickets
-    # sold_tickets = raw_data["data"].sold_tickets
+    # Validate event existance
+    found_event = EventsService.find_by_id(ticket_params[:event_id])
+    raise "Event not found" unless found_event
 
-    total_tickets = 100
-    sold_tickets = 70
+    tickets_summary = get_ticket_summary(ticket_params[:event_id])
+    total_tickets = tickets_summary["total_tickets"]
+    sold_tickets = tickets_summary["sold_tickets"]
 
-    if EventsService.find_by_id(ticket_params[:event_id]) != nil
-      new_ticket_report = TicketReport.create(capacity: total_tickets)
+    new_ticket_report = TicketReport.create(capacity: total_tickets)
 
-      current_date = Time.now
+    current_date = Time.now
 
-      format = Report.formats[ticket_params[:format]&.downcase]
+    format = Report.formats[ticket_params[:format]&.downcase]
 
-      new_report = new_ticket_report.create_report(
-        event_id: ticket_params[:event_id],
-        format: format,
-        sold_tickets: sold_tickets,
-        date: current_date,
-      )
+    new_report = new_ticket_report.create_report(
+      event_id: ticket_params[:event_id],
+      format: format,
+      sold_tickets: sold_tickets,
+      date: current_date,
+    )
 
-      # log = new_ticket_report.report.report_logs.create(status: :created, user_id: ticket_params[:user_id])
-      case ticket_params[:format]&.downcase
-      when "json"
-        {
-        id: new_ticket_report.id,
-        total_tickets: new_ticket_report.capacity,
-        event_id: new_report.event_id,
-        format: new_report.format,
-        sold_tickets: new_report.sold_tickets,
-        date: new_report.date,
-        created_at: new_report.created_at
-            }
-      when "pdf"
-        pdf_data = generate_pdf(new_ticket_report, new_report)
+    new_ticket_report.report.report_logs.create(status: :created, user_id: ticket_params[:user_id])
+    case ticket_params[:format]&.downcase
+    when "json"
+      {
+      id: new_ticket_report.id,
+      total_tickets: new_ticket_report.capacity,
+      event_id: new_report.event_id,
+      format: new_report.format,
+      sold_tickets: new_report.sold_tickets,
+      date: new_report.date,
+      created_at: current_date
+          }
+    when "pdf"
+      pdf_data = generate_pdf(new_ticket_report, new_report)
 
-        {
-          ticket_report: {
-            id: new_ticket_report.id,
-            total_tickets: new_ticket_report.capacity,
-            event_id: new_report.event_id,
-            format: new_report.format,
-            sold_tickets: new_report.sold_tickets,
-            date: new_report.date,
-            created_at: new_report.created_at
-          },
-          pdf_data: pdf_data
-        }
-      when "csv"
-        {
-        success: true,
-        ticket_report: new_ticket_report,
-        report: new_report,
-        csv_data: generate_csv(new_ticket_report, new_report) # Generar CSV
+      {
+        ticket_report: {
+          id: new_ticket_report.id,
+          total_tickets: new_ticket_report.capacity,
+          event_id: new_report.event_id,
+          format: new_report.format,
+          sold_tickets: new_report.sold_tickets,
+          date: new_report.date,
+          created_at: new_report.created_at
+        },
+        pdf_data: pdf_data
       }
-      end
-    else
-      raise "Event not found"
+    when "csv"
+      {
+      success: true,
+      ticket_report: new_ticket_report,
+      report: new_report,
+      csv_data: generate_csv(new_ticket_report, new_report) # Generar CSV
+    }
     end
   end
 
@@ -108,9 +105,9 @@ class TicketsService
     end.render
   end
 
-  def get_ticket_summary
-    uri = URI("asdas")
-    response_tkts = Net::HTTP.get(uri)
+  def self.get_ticket_summary(event_id)
+    uri = URI.parse("#{ENV.fetch("TICKET_URL")}/events/#{event_id}/tickets/summary")
+    response_tkts = Net::HTTP.get_response(uri)
     JSON.parse(response_tkts.body)
   end
 end
